a23191c95e5b7c15b675e0feb44e2141a6cdf17b
SepVeneto <sepveneto@qq.com>
2022-09-23 14:40:45 +0800

    build: remove bin

--- editor/vue.config.js
+++ editor/vue.config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 module.exports = defineConfig({
   assetsDir: 'deign-static',
   publicPath: '',
-  outputDir: 'editor',
+  outputDir: '../cli/editor',
   devServer: {
     port: 8082,
     proxy: {
