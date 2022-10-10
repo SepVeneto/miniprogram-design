1526fca77d3f0575888c3310208a20852025456a
SepVeneto <sepveneto@qq.com>
2022-10-10 15:20:25 +0800

    chore: add demo

---
+++ docs/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div id="design-container" />
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { useDesign } from '@mpd/core'
+import { config, widgets, schema } from './config'
+async function init() {
+  const [getData, setData] = await useDesign(
+    '#design-container',
+    { port: 8090, url: window.location.origin + '/miniprogram-design/editor' }
+  )
+  setData({
+    config,
+    schema,
+    widgets,
+  })
+}
+init()
+</script>
