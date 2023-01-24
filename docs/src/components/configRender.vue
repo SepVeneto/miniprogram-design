6277e48a1426f6c845f73ed131a2172318be5863
SepVeneto <sepveneto@qq.com>
2023-01-24 18:30:00 +0800

    feat(docs): add config render

---
+++ docs/src/components/configRender.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="configView" />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent, shallowRef, watch } from 'vue';
+
+const props = {
+  type: {
+    type: String,
+    required: true,
+  },
+};
+const configView = shallowRef();
+
+watch(() => props.type, (type) => {
+  configView.value = defineAsyncComponent(
+    () => import(`@/config/${type}.view.vue`),
+  );
+}, { immediate: true });
+</script>
