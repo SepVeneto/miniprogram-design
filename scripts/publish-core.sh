4e55116e8ee7c8fe9c286c00a2c22c24ce7e00f5
SepVeneto <sepveneto@qq.com>
2023-04-20 13:46:16 +0800

    ci(core): publish update version from tag

--- scripts/publish-core.sh
+++ scripts/publish-core.sh
@@ -6,6 +6,8 @@ pnpm i --frozen-lockfile --registry=https://registry.npmmirror.com
 
 cd packages/core
 
+sed -i "s/\"version\": \"0.2.0\",/\"version\": \"$TAG_VERSION\",/" package.json
+
 pnpm build
 
 pnpm publish --no-git-checks --access public
