cbf8d4f2574d052112b2de8d4536cb41923e5825
SepVeneto <sepveneto@qq.com>
2023-08-18 17:11:34 +0800

    style(hooks): eslint

--- packages/hooks/src/use-normalize-style.ts
+++ packages/hooks/src/use-normalize-style.ts
@@ -1,31 +1,31 @@
-import { ComputedRef, computed } from 'vue';
-import type { CSSProperties } from 'vue';
+import { computed } from 'vue'
+import type { CSSProperties, ComputedRef } from 'vue'
 type Style = Partial<Record<keyof CSSProperties, string | number>>
-type ReturnStyle<T extends Style> = { transition: string } & T;
-export function useNormalizeStyle<T extends Style> (
+type ReturnStyle<T extends Style> = { transition: string } & T
+export function useNormalizeStyle<T extends Style>(
   style: T,
 ): ComputedRef<ReturnStyle<T>> {
   return computed(() => {
-    const _style = normalizeStyle(style);
+    const _style = normalizeStyle(style)
 
     return {
       transition: 'inherit',
       ..._style,
-    } as ReturnStyle<T>;
-  });
+    } as ReturnStyle<T>
+  })
 }
 
-export function normalizeStyle (style: Style) {
+export function normalizeStyle(style: Style) {
   return Object.entries(style).reduce<Partial<CSSProperties>>(
     (obj, _style) => {
-      const [key, value] = _style;
+      const [key, value] = _style
       if (typeof value === 'number') {
         // @ts-expect-error: value is not a number
-        obj[key] = `${value}px`;
+        obj[key] = `${value}px`
       } else {
         // @ts-expect-error: value is not a number
-        obj[key] = value;
+        obj[key] = value
       }
-      return obj;
-    }, {} as Partial<CSSProperties>);
+      return obj
+    }, {} as Partial<CSSProperties>)
 }
