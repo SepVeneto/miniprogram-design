8646e5928a344346b8d46096d2ac477ee761781c
SepVeneto <sepveneto@qq.com>
2023-09-09 16:27:25 +0800

    chore: clear

--- docs-widgets/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import config from './default';
-import schema from './schema.json';
-import widgets from './widgets.json';
-
-export {
-  config,
-  schema,
-  widgets,
-};
--- docs-widgets/src/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './use-normalize-style'
-export * from './use-state'
