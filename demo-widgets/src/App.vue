c112184ce1bf8c676ade73b9f655494c2831edcc
SepVeneto <sepveneto@qq.com>
2024-04-10 20:15:38 +0800

    chore: update demo

--- demo-widgets/src/App.vue
+++ demo-widgets/src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <section>
     <div id="design-container" />
-    <micro-app
+    <div>test</div>
+    <!-- <micro-app
       url="http://localhost:8082"
       name="demo"
-    />
+    /> -->
   </section>
 </template>
+
+<script lang="ts" setup>
+import { useDesign } from '@sepveneto/mpd-core';
+
+useDesign('#design-container', {
+  url: 'http://localhost:8082',
+  name: 'demo',
+  inline: true,
+  data: {
+    remoteUrl: 'http://localhost:8090',
+    config: { globalConfig: {}, body: {}},
+  }
+})
+</script>
