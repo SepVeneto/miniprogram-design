2ea6644c99f2c0f302819a98e3b81dc7aa5c8d14
SepVeneto <sepveneto@qq.com>
2025-05-14 13:51:03 +0800

    fix(editor): 部分情况富文本全屏样式异常

--- packages/schema/src/components/editor.vue
+++ packages/schema/src/components/editor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="border: 1px solid #ccc; z-index: 10;">
+  <div style="border: 1px solid #ccc; z-index: 999;">
     <Toolbar
       style="border-bottom: 1px solid #ccc"
       :editor="editorRef"
@@ -10,19 +10,19 @@
       style="height: 500px; overflow-y: hidden"
       :default-config="editorConfig"
       :mode="mode"
-      v-bind="$attrs"
+      v-bind="{ ...$attrs, style: '' }"
       @on-created="onCreated"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import '@wangeditor/editor/dist/css/style.css';
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
-import { IDomEditor, IToolbarConfig, DomEditor } from '@wangeditor/editor';
-import { shallowRef, onBeforeUnmount } from 'vue';
+import '@wangeditor/editor/dist/css/style.css'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import type { IDomEditor, IToolbarConfig } from '@wangeditor/editor'
+import { onBeforeUnmount, shallowRef } from 'vue'
 
-const editorRef = shallowRef<IDomEditor>();
+const editorRef = shallowRef<IDomEditor>()
 
 // onMounted(() => {
 
@@ -36,7 +36,7 @@ const toolbarConfig: Partial<IToolbarConfig> = {
     'codeBlock',
     'group-indent',
   ],
-};
+}
 const editorConfig = {
   placeholder: '请输入内容...',
   MENU_CONF: {
@@ -45,19 +45,20 @@ const editorConfig = {
         // file 即选中的文件
         // 自己实现上传，并得到图片 url alt href
         // 最后插入图片
-        const url = await window.microApp?.getData()?.upload?.({ file });
-        insertFn(url);
+        // @ts-expect-error: remote upload
+        const url = await window.microApp?.getData()?.upload?.({ file })
+        insertFn(url)
       },
     },
   },
-};
-const mode = 'default';
+}
+const mode = 'default'
 
 onBeforeUnmount(() => {
-  editorRef.value?.destroy?.();
-});
+  editorRef.value?.destroy?.()
+})
 
-async function onCreated (editor: IDomEditor) {
-  editorRef.value = editor;
+async function onCreated(editor: IDomEditor) {
+  editorRef.value = editor
 }
 </script>
